78700295ff510f179732dad7fa051a990d8fd884
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2022-02-12 08:33:43 +0000

    update celery task logic
    Signed-off-by: Ashesh3 <3626859+Ashesh3@users.noreply.github.com>

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,50 +1,42 @@
-from pytz import timezone
-from tasks.models import STATUS_CHOICES, Task, UserPreferences
-from task_manager.celery import app
-from django.core.mail import send_mail
 from datetime import datetime, timedelta
+from smtplib import SMTPException
 
 from celery.decorators import periodic_task
+from django.core.mail import send_mail
 
+from tasks.models import STATUS_CHOICES, Task, UserPreferences
+from django.db.models import Q
 from django.utils.timezone import make_aware
 
 
-@app.task
-def process_email(user):
+def get_email_content(user):
+    all_tasks = Task.objects.filter(user=user, deleted=False)
     email_content = f"Hello {user.username}!\n\n"
     email_content += "Here is your tasks summary:\n"
-    all_tasks = Task.objects.filter(user=user, deleted=False)
     for status in STATUS_CHOICES:
         tasks = all_tasks.filter(status=status[0])
         if tasks.exists():
             email_content += f"\n{len(tasks)} {status[1].lower()} task(s).\n"
     email_content += "\n\n"
     email_content += "Thank you!"
-    send_mail("Tasks summary", email_content, "tasks@task_manager.org", [user.email], fail_silently=False,)
+
     return email_content
-
-
-@app.task
-def process_missing_users():
-    users = UserPreferences.objects.filter(reminder_enabled=True, last_sent__lte=make_aware(datetime.now() - timedelta(hours=24)))
-    for user in users:
-        user.last_sent = make_aware(datetime.now())
-        user.save()
-        process_email.delay(user.user)
-    return users
 
 
 @periodic_task(run_every=timedelta(seconds=30))
 def send_reports():
-    print("\nProcessing celery task")
-    current_time = datetime.now()
-    matching_users = UserPreferences.objects.filter(reminder_enabled=True, reminder_time__range=(current_time - timedelta(seconds=30), current_time))
-    if len(matching_users) == 0:
-        print("No matching users found")
-    for user_prefs in matching_users:
-        print("Sending to: ", user_prefs.user)
-        user_prefs.last_sent = make_aware(datetime.now())
-        user_prefs.save()
-        process_email.delay(user_prefs.user)
+    new_and_missing_users = new_and_missing_users = UserPreferences.objects.filter(Q(reminder_enabled=True) & (
+        Q(last_sent__lte=make_aware(datetime.now() - timedelta(hours=24))) | Q(last_sent=None)))
 
-    process_missing_users.delay()
+    if len(new_and_missing_users) == 0:
+        print("No matching users found")
+
+    for user_prefs in new_and_missing_users:
+        print("Processing", user_prefs.user)
+        try:
+            email_content = get_email_content(user_prefs.user)
+            send_mail("Tasks summary", email_content, "tasks@task_manager.org", [user_prefs.user.email], fail_silently=False,)
+            user_prefs.last_sent = make_aware(datetime.now())
+            user_prefs.save()
+        except SMTPException as e:
+            print("Error sending email", e)
